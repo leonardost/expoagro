d38240737bcdc6ded81ea2173e2ded64bcd77799
renatamt <renatamt@users.noreply.github.com>
2014-04-29 21:08:08 -0300

    Alteração no script para criação do BD. Script funcionando!

--- sql/script.sql
+++ sql/script.sql
@@ -1,43 +1,12 @@
-CREATE DATABASE expoagro;
+--CREATE DATABASE expoagro;
 
-DROP TABLE pontuacao_premiacao;
-DROP TABLE premiacao;
-DROP TABLE pontuacao;
-DROP TABLE categoria;
-DROP TABLE produtor_produto;
-DROP TABLE produto;
-DROP TABLE produtor;
-
-
-CREATE TABLE produtor (
-    id SERIAL,
-    nome TEXT NOT NULL,
-    endereco TEXT
-);
-ALTER TABLE produtor ADD CONSTRAINT produtor_pk PRIMARY KEY (id);
-ALTER TABLE produtor ADD CONSTRAINT produtor_uq UNIQUE (nome)
-
-CREATE TABLE produto (
-    id SERIAL,
-    nome TEXT NOT NULL,
-    id_categoria INTEGER
-);
-ALTER TABLE produto ADD CONSTRAINT produto_pk PRIMARY KEY (id);
-ALTER TABLE produto ADD CONSTRAINT produto_uq UNIQUE (nome);
-ALTER TABLE produto ADD CONSTRAINT produto_fk FOREIGN KEY (id_categoria) 
-	REFERENCES categoria (id) ON UPDATE CASCADE ON DELETE SET NULL;
-
-CREATE TABLE produtor_produto (
-    id_produtor INTEGER,
-    id_produto INTEGER,
-    classificacao CHAR(1)
-);
-ALTER TABLE produtor_produto ADD CONSTRAINT produtor_produto_pk PRIMARY 
-	KEY (id_produtor, id_produto);
-ALTER TABLE produtor_produto ADD CONSTRAINT produtor_produto_fk1 FOREIGN
-	KEY (id_produtor) REFERENCES produtor (id);
-ALTER TABLE produtor_produto ADD CONSTRAINT produtor_produto_fk2 FOREIGN
-	KEY (id_produto) REFERENCES produto (id);
+--DROP TABLE produtor_produto;
+--DROP TABLE produto;
+--DROP TABLE produtor;
+--DROP TABLE pontuacao_premiacao;
+--DROP TABLE premiacao;
+--DROP TABLE pontuacao;
+--DROP TABLE categoria;
 
 CREATE TABLE categoria (
 	id SERIAL,
@@ -79,4 +48,33 @@ ALTER TABLE pontuacao_premiacao ADD CONSTRAINT pontuacao_premiacao_fk1
 ALTER TABLE pontuacao_premiacao ADD CONSTRAINT pontuacao_premiacao_fk2
 	FOREIGN KEY (id_premiacao) REFERENCES premiacao (id) ON UPDATE
 	CASCADE ON DELETE CASCADE;
-	
+
+CREATE TABLE produtor (
+    id SERIAL,
+    nome TEXT NOT NULL,
+    endereco TEXT
+);
+ALTER TABLE produtor ADD CONSTRAINT produtor_pk PRIMARY KEY (id);
+ALTER TABLE produtor ADD CONSTRAINT produtor_uq UNIQUE (nome);
+
+CREATE TABLE produto (
+    id SERIAL,
+    nome TEXT NOT NULL,
+    id_categoria INTEGER
+);
+ALTER TABLE produto ADD CONSTRAINT produto_pk PRIMARY KEY (id);
+ALTER TABLE produto ADD CONSTRAINT produto_uq UNIQUE (nome);
+ALTER TABLE produto ADD CONSTRAINT produto_fk FOREIGN KEY (id_categoria) 
+	REFERENCES categoria (id) ON UPDATE CASCADE ON DELETE SET NULL;
+
+CREATE TABLE produtor_produto (
+    id_produtor INTEGER,
+    id_produto INTEGER,
+    classificacao CHAR(1)
+);
+ALTER TABLE produtor_produto ADD CONSTRAINT produtor_produto_pk PRIMARY 
+	KEY (id_produtor, id_produto);
+ALTER TABLE produtor_produto ADD CONSTRAINT produtor_produto_fk1 FOREIGN
+	KEY (id_produtor) REFERENCES produtor (id);
+ALTER TABLE produtor_produto ADD CONSTRAINT produtor_produto_fk2 FOREIGN
+	KEY (id_produto) REFERENCES produto (id);	
