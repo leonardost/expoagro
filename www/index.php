ac125dec5ee656d36ee87bb5216136d0a67e207b
leonardost <leonardost@users.noreply.github.com>
2014-05-06 10:42:15 -0300

    Vagrantfile e página index.php modificados

--- www/index.php
+++ www/index.php
@@ -1,3 +1,20 @@
+<?php
+
+	// Apenas testando, MD5 não deve ser usado para processar senhas
+	$md5hash = '5166bc207a30cd8d95ab9cfd532b0a7e';
+	$salt1 = 'TAKpH5GM';
+	$salt2 = 'k6gwCmFh';
+
+	if (!empty($_POST['senha'])) {
+		if (md5(md5(md5($salt1 . $_POST['senha'] . $salt2))) === $md5hash) {
+			die("Logado!");
+		}
+		else {
+			die("Senha incorreta! Tente novamente.");
+		}
+	}
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,7 +23,12 @@
 </head>
 <body>
 
-    <a href="teste.php">Página de testes</a>
+    <a href="index.php">Página de testes</a>
+    
+    <form action="index.php" method="post">
+		<input name="senha" type="password">
+		<input name="submit" type="submit" value="Fazer login">
+    </form>
 
 </body>
 </html>
