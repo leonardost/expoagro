5f389ce7b519077ba1d26083218fc3f0a7ebbf66
leonardost <leonardost@users.noreply.github.com>
2014-07-17 01:39:12 -0300

    Removendo arquivos de commits antigos que já deveriam ter sido removidos

--- www/deslogar.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    session_start();
-    session_destroy();
-    // http://stackoverflow.com/questions/9971626/session-variables-do-not-get-destroy-even-using-session-destroy
-    $_SESSION = array();
-
-    header('Location: index.php');
-    exit;
